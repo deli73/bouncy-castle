5277a86be88baca690cce86fec7a93a993604e13
Sunroses <deli73@users.noreply.github.com>
2022-08-12 18:35:27 -0700

    add blocks, no textures yet

---
+++ src/main/java/xyz/sunrose/bouncycastle/BouncyBlock.java
@@ -0,0 +1,9 @@
+package xyz.sunrose.bouncycastle;
+
+import net.minecraft.block.Block;
+
+public class BouncyBlock extends Block {
+	public BouncyBlock(Settings settings) {
+		super(settings);
+	}
+}
--- src/main/java/xyz/sunrose/bouncycastle/BouncyCastle.java
+++ src/main/java/xyz/sunrose/bouncycastle/BouncyCastle.java
@@ -1,18 +1,45 @@
 package xyz.sunrose.bouncycastle;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BouncyCastle implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final String MODID = "bouncycastle";
+	public static final Logger LOGGER = LoggerFactory.getLogger("Bouncy Castle");
+
+	public static List<Block> BOUNCY_BLOCKS = new ArrayList<>();
+	public static List<Item> BOUNCY_BLOCK_ITEMS = new ArrayList<>();
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		for(DyeColor color : DyeColor.values()){
+			String name = color.asString()+"_bouncy_block";
+			Identifier ID = new Identifier(MODID, name);
+			final Block BLOCK = Registry.register(
+					Registry.BLOCK, ID,
+					new BouncyBlock(QuiltBlockSettings.of(Material.WOOL))
+			);
+			final Item ITEM = Registry.register(
+					Registry.ITEM, ID,
+					new BlockItem(BLOCK, new QuiltItemSettings().group(ItemGroup.DECORATIONS))
+			);
+			BOUNCY_BLOCKS.add(BLOCK);
+			BOUNCY_BLOCK_ITEMS.add(ITEM);
+		}
 	}
 }

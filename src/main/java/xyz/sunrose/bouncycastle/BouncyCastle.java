65aa80b1987f6c0b3950da73c9bec47f0130f1bb
Sunroses <deli73@users.noreply.github.com>
2022-08-13 23:21:06 -0700

    fix logger??

--- src/main/java/xyz/sunrose/bouncycastle/BouncyCastle.java
+++ src/main/java/xyz/sunrose/bouncycastle/BouncyCastle.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class BouncyCastle implements ModInitializer {
 	public static final String MODID = "bouncycastle";
-	public static final Logger LOGGER = LoggerFactory.getLogger("Bouncy Castle");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	public static List<Block> BOUNCY_BLOCKS = new ArrayList<>();
 	public static List<Item> BOUNCY_BLOCK_ITEMS = new ArrayList<>();
